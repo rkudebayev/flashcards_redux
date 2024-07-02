e5884f9355f70ac073c478652b4edece2d2e0cf0
Rassul Kudebayev <rasulito92@MacBook-Air-Rassul.local>
2024-07-02 20:42:36 +0500

    first commit

---
+++ src/features/cards/Card.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectCards } from "./cardSlice";
+// import selector
+
+export default function Card({ id }) {
+  const card = useSelector(selectCards(id)); // replace this with a call to your selector to get a card by id
+  
+  const [flipped, setFlipped] = useState(false);
+
+  return (
+    <li>
+      <button className="card" onClick={(e) => setFlipped(!flipped)}>
+        {flipped ? card.back : card.front}
+      </button>
+    </li>
+  );
+}
---
+++ src/features/topics/topicsSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const topicsSlice = createSlice({
+    name: 'topics',
+    initialState: {
+        topics: {}
+    },
+    reducers: {
+        addTopic: (state, action) => {
+            const {id, name, icon} = action.payload;
+            state.topics[id] = {
+                id: id,
+                name: name,
+                icon: icon,
+                quizIds: [],
+            }
+        }
+    },
+    extraReducers: {
+        'quizzes/addQuizz': (state, action) => {
+                const {id, topicId} = action.payload;
+                state.topics[topicId].quizIds.push(id);
+            }
+    }
+})
+
+
+export const selectTopics = (state) => state.topics.topics;
+
+export const {addTopic, addQuizId} = topicsSlice.actions;
+
+export default topicsSlice.reducer;
+
+
